915e91eafec1a41c4eef2d84e7c5f37fc95b5339
Rake Yang <fenglaijun@gmail.com>
2021-11-10 18:14:13 +0800

    兼容linux

--- Sources/Proto/ProtoDevice.swift
+++ Sources/Proto/ProtoDevice.swift
@@ -41,7 +41,6 @@ public struct ProtoDevice: Codable {
             return String(cString: machine)
         }()
         simulator = TARGET_OS_SIMULATOR == 1
-        ipAddrs = ipAddress()
         #elseif os(Linux)
         let info = ProcessInfo.processInfo
         name = Host.current().localizedName ?? "Unknown"
@@ -55,41 +54,6 @@ public struct ProtoDevice: Codable {
         osVersion = UIDevice.current.systemVersion
         modelName = UIDevice.current.localizedModel
         simulator = TARGET_OS_SIMULATOR == 1
-        ipAddrs = ipAddress()
         #endif
-    }
-    
-    private func ipAddress() -> [String : [String : String]]? {
-        var ipv4: [String : String] = [:]
-        var ipv6: [String : String] = [:]
-        var ifaddr : UnsafeMutablePointer<ifaddrs>? = nil
-        if getifaddrs(&ifaddr) == 0 {
-            var ptr = ifaddr
-            while (ptr != nil) {
-                let flags = Int32(ptr!.pointee.ifa_flags)
-                var addr = ptr!.pointee.ifa_addr.pointee
-                if (flags & (IFF_UP|IFF_RUNNING|IFF_LOOPBACK)) == (IFF_UP|IFF_RUNNING) {
-                    if addr.sa_family == UInt8(AF_INET) || addr.sa_family == UInt8(AF_INET6) {
-                        var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-                        if (getnameinfo(&addr, socklen_t(addr.sa_len), &hostname, socklen_t(hostname.count),nil, socklen_t(0), NI_NUMERICHOST) == 0) {
-                            if let address = String(validatingUTF8:hostname) {
-                                let name = String(cString: ptr!.pointee.ifa_name)
-                                if name.hasPrefix("en") {
-                                    if addr.sa_family == AF_INET {
-                                        ipv4[name] = address
-                                    } else {
-                                        ipv6[name] = address
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                ptr = ptr!.pointee.ifa_next
-            }
-            freeifaddrs(ifaddr)
-        }
-        
-        return ["ipv4": ipv4, "ipv6" : ipv6]
     }
 }
